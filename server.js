765edfb3b8e5c872278ab2c77c70257548aa1d9f
philmaxsnr <philmaxsnr@gmail.com>
2020-03-23 12:21:47 +0300

    update

--- server.js
+++ server.js
@@ -175,9 +175,7 @@ try {
         if (error) {
             console.log(error);
         }
-        console.log('Message sent: %s', info.messageId);
-        // Preview only available when sending through an Ethereal account
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+       console.log(info)
     });
 } catch (error) {
     console.log(error)
