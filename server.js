6b45fab3c09b561948eb7776e248b87f695dcb96
philmaxsnr <philmaxsnr@gmail.com>
2020-03-23 12:13:24 +0300

    update

--- server.js
+++ server.js
@@ -156,7 +156,7 @@ var sendEmail = function (rec, body, subject) {
             host: "smtp.mailtrap.io",
             port: 2525,
             auth: {
-                user: 'royalehealthclubkaren@gmail.com', // generated ethereal user
+                user: 'rch', // generated ethereal user
                 pass: 'Philip@ademba4' // g enerated ethereal password
             },
             debug: false,
